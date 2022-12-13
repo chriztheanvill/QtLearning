696c51d944bb7afb3322874f8653db1a57e26b78
chriztheanvill <chriztheanvill@gmail.com>
2022-12-13 15:20:11 -0600

    Updating_

--- Learn_6_41/widget.cpp
+++ Learn_6_41/widget.cpp
@@ -1,11 +1,16 @@
 #include "widget.h"
+#include <QLabel>
+#include <QLineEdit>
 
-Widget::Widget(QWidget *parent)
-    : QWidget(parent)
-{
+Widget::Widget(QWidget *parent) : QWidget(parent) {
+  // First Name
+  QLabel *firstNameLabel = new QLabel("FirstName", this);
+  firstNameLabel->setMinimumSize(70, 50);
+  firstNameLabel->move(10, 10);
+
+  QLineEdit *firstNameLineEdit = new QLineEdit(this);
+  firstNameLineEdit->setMinimumSize(200, 50);
+  firstNameLineEdit->move(100, 10);
 }
 
-Widget::~Widget()
-{
-}
-
+Widget::~Widget() {}
