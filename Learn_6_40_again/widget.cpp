70af33abc515242b55df477b8a8af7e4eb8f434f
chriztheanvill <chriztheanvill@gmail.com>
2022-12-16 19:40:10 -0600

    Updating_

--- Learn_6_40_again/widget.cpp
+++ Learn_6_40_again/widget.cpp
@@ -3,25 +3,23 @@
 #include <QPushButton>
 
 Widget::Widget(QWidget *parent) : QWidget(parent) {
-  // Button 1
-  QFont buttonFont("Times", 20, QFont::Bold);
+  // Clicked Signal
+  QFont buttonFront("Times", 20, QFont::Bold);
   QPushButton *button1 = new QPushButton("Button1", this);
   button1->setMinimumSize(200, 100);
-  button1->setFont(buttonFont);
+  button1->setFont(buttonFront);
   connect(button1, &QPushButton::clicked,
           [=]() { qDebug() << "Button1 clicked"; });
 
   // Button 2
   QPushButton *button2 = new QPushButton("Button2", this);
   button2->setMinimumSize(200, 100);
-  button2->setFont(buttonFont);
   button2->move(200, 0);
+  button2->setFont(buttonFront);
   connect(button2, &QPushButton::pressed,
-          [=]() { qDebug() << "Button2 pressed ++++"; });
+          [=]() { qDebug() << "Button2 pressed"; });
   connect(button2, &QPushButton::released,
-          [=]() { qDebug() << "Button2 released ----"; });
-  connect(button2, &QPushButton::clicked,
-          [=]() { qDebug() << "Button2 clicked"; });
+          [=]() { qDebug() << "Button2 released"; });
 }
 
 Widget::~Widget() {}
