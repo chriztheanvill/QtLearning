524942b999c30b632771cdd9cbf19ac7c3a51511
chriztheanvill <chriztheanvill@gmail.com>
2022-12-16 19:43:42 -0600

    Updating_

--- Learn_6_40_again/main.cpp
+++ Learn_6_40_again/main.cpp
@@ -1,11 +1,12 @@
+/* Re-hice esta parte, por que no imprimia qDebug() */
+
 #include "widget.h"
 
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    Widget w;
-    w.show();
-    return a.exec();
+int main(int argc, char *argv[]) {
+  QApplication a(argc, argv);
+  Widget w;
+  w.show();
+  return a.exec();
 }
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # QtLearning
+## Nota:
+Si no muestra nada qDebug() en consola.
+
+   	vim  ~/.config/QtProject/qtlogging.ini
+
+
+```sh
+[Rules]
+*.debug=true
+qt.*.debug=false
+```
