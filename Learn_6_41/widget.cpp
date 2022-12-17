6580b3a3769a2ea89c2a199daa1fc179fce05839
chriztheanvill <chriztheanvill@gmail.com>
2022-12-16 20:05:43 -0600

    Updating_

--- Learn_6_41/widget.cpp
+++ Learn_6_41/widget.cpp
@@ -11,6 +11,18 @@ Widget::Widget(QWidget *parent) : QWidget(parent) {
   QLineEdit *firstNameLineEdit = new QLineEdit(this);
   firstNameLineEdit->setMinimumSize(200, 50);
   firstNameLineEdit->move(100, 10);
+
+  // Last name
+  QLabel *lastNameLabel = new QLabel("LastName", this);
+  lastNameLabel->setMinimumSize(70, 50);
+  lastNameLabel->move(10, 70);
+
+  QLineEdit *lastNameLineEdit = new QLineEdit(this);
+  lastNameLineEdit->setMinimumSize(200, 50);
+  lastNameLineEdit->move(100, 70);
+
+  QLabel *ageNameLabel = new QLabel("Age", this);
+  ageNameLabel->setMinimumSize(200, 50);
 }
 
 Widget::~Widget() {}
